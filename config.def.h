e41a3f2e47cba3080183df7447ca184441eb879d
syaofox <syaofox@gmail.com>
2022-03-03 18:02:10 +0800

    添加鼠标右键复制绑定

--- config.def.h
+++ config.def.h
@@ -190,10 +190,14 @@ static MouseShortcut mshortcuts[] = {
 	{ ShiftMask,            Button4, kscrollup,      {.i = 1} },
 	{ ShiftMask,            Button5, kscrolldown,    {.i = 1} },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
-	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
-	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
-	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
-	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
+
+
+	// 鼠标右键粘贴，shift复制
+	{ XK_ANY_MOD,           Button3, clippaste,       {.i = 0},      1 },
+	// { ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
+	// { XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
+	// { ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
+	// { XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 };
 
 /* Internal keyboard shortcuts. */
