f4b7e097dff1d8b9a6795374b4a6009d7481ad6a
syaofox <syaofox@gmail.com>
2022-03-15 20:26:23 +0800

    update

--- config.def.h
+++ config.def.h
@@ -218,8 +218,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ XK_ANY_MOD,            XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ XK_ANY_MOD,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
